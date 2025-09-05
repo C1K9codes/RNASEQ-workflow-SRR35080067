accf5777579e459cb1e3a4ad478268703974f587
Charvi Khanna <charvikhanna02@gmail.com>
2025-09-05 13:59:11 +0530

    fastqc --results.sh

--- fastq/fastqc --results.md
+++
@@ -1,4 +0,0 @@
-fastqc --results
-
-## file://wsl.localhost/Debian/home/charvis/fastqc_reports/SRR35080067_1_fastqc.html
-## file://wsl.localhost/Debian/home/charvis/fastqc_reports/SRR35080067_2_fastqc.html
---
+++ fastq/fastqc --results.sh
@@ -0,0 +1,8 @@
+fastqc --results
+
+### forward reads
+## file://wsl.localhost/Debian/home/charvis/fastqc_reports/SRR35080067_1_fastqc.html
+
+### reversed reads
+## file://wsl.localhost/Debian/home/charvis/fastqc_reports/SRR35080067_2_fastqc.html
+
