db3ab14b89d9c678e1f83497bca91f79d1e825b3
Charvi Khanna <charvikhanna02@gmail.com>
2025-09-06 20:55:16 +0530

    Update snakemake.sh

--- snakemake.sh
+++ snakemake.sh
@@ -1,2 +1,7 @@
 wget "https://github.com/conda-forge/miniforge/releases/download/25.3.1-0/Miniforge3-25.3.1-0-Linux-x86_64.sh"
 miniforge is the package having python, mamba and conda
+
+
+bash Miniforge3-25.3.1-0-Linux-x86_64.sh
+
+mamba --version
